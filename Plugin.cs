d69931b0081d9c2c66c1eaa53c529250c8fc7ef3
phofers <adamkuc915@gmail.com>
2025-07-09 18:51:39 -0500

    first commit

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace FPSUnlimiter;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Debug.Log("This game is MODDED! FPSUnlimiter is loaded.");
+
+        
+    }
+
+
+}
