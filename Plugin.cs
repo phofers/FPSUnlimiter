ce2d7e089992ab6f6bf3f68d6c4b289b29ba8dcb
phofers <adamkuc915@gmail.com>
2025-07-09 20:36:42 -0500

    Create patch to unlimit game FPS

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 using UnityEngine;
 
 namespace FPSUnlimiter;
@@ -14,10 +15,29 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        // I think (???) Debug.Log prints to unity player.log file but really am unsure. Whatever, it doesn't matter much, it's still logging in the bepinex log.
         Debug.Log("This game is MODDED! FPSUnlimiter is loaded.");
+        Harmony harmony = new Harmony("phofers");
+        harmony.PatchAll();
+    }
 
+    [HarmonyPatch(typeof(FPSLimiter), "Awake")]
+    public class FPSPatch
+    {
+        static bool Prefix()
+        {
+
+
+            Application.targetFrameRate = -1;
+            QualitySettings.vSyncCount = 0;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} FPSLimiter patch applied!");
+            return false;
+            
+        }
         
     }
+    
+
 
 
 }
---
+++ obj/Release/net472/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FPSUnlimiter
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "FPSUnlimiter";
+        public const string PLUGIN_NAME = "FPSUnlimiter";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
